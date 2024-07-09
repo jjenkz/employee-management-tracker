9283543be426b1d0508a6bdd9032312844e4dc8c
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-07-08 21:15:04 -0700

    debugged code where I had a typescript error

--- config/connection.js
+++ config/connection.js
@@ -4,27 +4,27 @@ const { Client } = require("pg");
 
 require("dotenv").config();
 
-const client = New Client ({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 const schemaSql = path.join(__dirname, "./db/schema.sql");
 const seedsSql = path.join(__dirname, "./db/seeds.sql");
 
 const initDatabase = async () => {
-    try {
-        const schema = fs.readFileSync(schemaSql, "utf8");
-        const seeds = fs.readFileSync(seedsSql, "utf8");
-        await client.connect(schema);
-        await client.query(seeds);
-        console.log("Database initialized");
-    } catch (err) {
-        console.error("Error initializing database", err);
-    }
+  try {
+    const schema = fs.readFileSync(schemaSql, "utf8");
+    const seeds = fs.readFileSync(seedsSql, "utf8");
+    await client.connect(schema);
+    await client.query(seeds);
+    console.log("Database initialized");
+  } catch (err) {
+    console.error("Error initializing database", err);
+  }
 };
 
-module.exports = {client, initDatabase};
+module.exports = { client, initDatabase };
